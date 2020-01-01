258913cf5b3c0b22328386765dc2f94f220b92a5
Anna Levenberg <alevenb1@binghamton.edu>
2019-12-31 21:03:06 -0500

    Add data folder with scraped data and scraper writes to csv

--- scraper.py
+++ scraper.py
@@ -3,7 +3,6 @@ import requests
 import re
 import csv
 import os
-import os.path
 import csv 
 import argparse
 import datetime
@@ -56,17 +55,20 @@ def get_clues(url):
 
 def write_to_csv(crossword_dict, file_name):
     # Make directory in file it is run in
-    directory = "./data/"
+    directory = "data"
     my_path = os.path.abspath(os.path.dirname(__file__))
-    print(my_path)
+    data_path = os.path.join(my_path, directory)
 
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
 
-# csv_file = date
-# csvfile = open(csv_file + '.csv', 'wb')
-# writer = csv.writer(csvfile)
-# path = os.path.join(my_path, "./data/test.csv")
-# with open(path) as f:
-#     test = list(csv.reader(f))
+    csv_path = os.path.join(data_path, file_name)
+    with open(csv_path, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for key, value in crossword_dict.items():
+            for clue, answer in value.items():
+                writer.writerow([key, clue, answer])
+
 def main():
     # Command line argument parsing
     parser = argparse.ArgumentParser(description='A script that scrapes https://nyxcrossword.com/ for the crossword clues and answers in the specified date range')
@@ -102,69 +104,16 @@ def main():
         
         url = urllib.parse.urljoin(base_url, ("/".join(url_date_items)))
 
-        # clues_dict = get_clues(url)
-        # crosswords[single_date.strftime("%Y-%m-%d")] = clues_dict
+        clues_dict = get_clues(url)
+        crosswords[single_date.strftime("%Y-%m-%d")] = clues_dict
+    
     if (args.start_date != args.end_date):
         file_name = "crossword-clues-from-" + str(args.start_date) + "-to-" + str(args.end_date) + ".csv"
     else: 
-        file_name = "crossword-clues-on-" + str(args.start_date) + ".csv"
+        file_name = "crossword-clues-from-" + str(args.start_date) + ".csv"
     print(file_name)
+
     # Write to csv
     write_to_csv(crosswords, file_name)
-    # print(crosswords)
-    #     urls.append(url)
-    # print(urls)
 
 main()
-
-
-# try:
-#     if not os.path.exists(directory):
-#         os.makedirs(directory)
-# except OSError:
-#     print ('Error: Creating directory. ' +  directory)
-    
-
-# Example
-# csv_columns = ['Date', 'Clue','Answer']
-
-# names = name_pattern.findall("The basic idea of regular expressions is we define a pattern (the “regular expression” or “regex”) that we want to match in a text string and then search in the string to return matches. Some of these patterns look pretty strange because they contain both the content we want to match and special characters that change how the pattern is interpreted. Regular expressions come up all the time when parsing string information and are a vital tool to learn at least at a basic level!")
-# print(clue_list)
-#     if re.match("^[A-Za-z0-9_-]:", clue):
-#         print(clue)
-#     # do something here
-
-# for linebreak in clue_list[0].find_all('br'):
-#     linebreak.extract()
-#     print(clue_list)
-# clue_list = list(map(lambda x: x.text.split("<br/>"), clue_list))
-# print(clue_list[0].)
-
-# content = list(map(lambda x: x.text, clue_list))
-
-# print(content)
-
-# print(clue_list.prettify())
-
-# write clue list to csv then graph data
-#import pandas as pd
-#daterange = pd.date_range(start_date, end_date)
-
-
-#url = "https://nyxcrossword.com/"
-
-#sp = "across_googlies"
-
-#content = list(map(lambda x: x.text, tags))
-
-#headers = requests.utils.default_headers()
-#headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
-
-#req = requests.get(url, headers)
-#soup = BeautifulSoup(req.content, 'html.parser')
-#print(soup.prettify())
-
-# source 
-# https://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python
-
-# https://stackoverflow.com/questions/42949449/using-pandas-date-range-to-generate-multiple-datetimes-two-dates-per-week
